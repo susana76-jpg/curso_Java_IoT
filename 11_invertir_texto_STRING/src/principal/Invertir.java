d407ea846ad40a8a5ad6c0084cb42aba17788a79
susana <srico@DESKTOP-G4DK7JT>
2020-07-12 11:31:40 +0200

    clase String

---
+++ 11_invertir_texto_STRING/src/principal/Invertir.java
@@ -0,0 +1,25 @@
+package principal;
+
+import java.util.Scanner;
+
+public class Invertir {
+
+	public static void main(String[] args) {
+		
+		Scanner sc=new Scanner(System.in);
+		String texto,textoFinal;
+		
+		System.out.println("Introduce un texto");
+		texto=sc.nextLine();
+		textoFinal=""; //inicializamos la variable auxiliar a cadena vacía
+		
+		
+		for(int i=texto.length()-1;i>=0;i--) {
+			textoFinal+=texto.charAt(i);
+		}
+		System.out.println("Texto al revés: "+textoFinal);
+		
+		sc.close();
+	}
+	
+}

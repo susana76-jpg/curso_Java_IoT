430893de08c945874670e213527a6de02965fdfb
susana <srico@DESKTOP-G4DK7JT>
2020-07-12 11:22:28 +0200

    Switch

---
+++ 04_notas_SWITCH/src/principal/MostrarNotas.java
@@ -0,0 +1,36 @@
+package principal;
+
+public class MostrarNotas {
+
+	public static void main(String[] args) {
+		int nota=6;
+
+		switch (nota) {
+			case 1:
+			case 2:
+			case 3:
+			case 4:
+				System.out.println("Suspenso");
+				break;
+			case 5:
+			case 6:
+				System.out.println("Aprobado");
+				break;
+			case 7:
+			case 8:
+				System.out.println("Notable");
+				break;
+			case 9:
+			case 10:
+				System.out.println("Sobresaliente");
+				break;
+	
+			default:
+				System.out.println("La nota no es valida");
+				break;
+		}
+
+
+	}
+
+}

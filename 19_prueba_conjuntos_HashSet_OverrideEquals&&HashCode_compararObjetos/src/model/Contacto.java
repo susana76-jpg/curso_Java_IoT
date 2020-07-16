a47994732eb946ad1c98f53aca422c2c38dd2022
susana <srico@DESKTOP-G4DK7JT>
2020-07-16 08:53:56 +0200

    HashSet.  Override métodos equals() y hashCode() de la clase Object

---
+++ 19_prueba_conjuntos_HashSet_OverrideEquals&&HashCode_compararObjetos/src/model/Contacto.java
@@ -0,0 +1,83 @@
+package model;
+
+	public class Contacto {
+		private String nombre;
+		private String email;
+		private int telefono;
+	
+	public Contacto(String nombre, String email, int telefono) {
+		this.nombre = nombre;
+		this.email = email;
+		this.telefono = telefono;
+	}
+	
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public int getTelefono() {
+		return telefono;
+	}
+	public void setTelefono(int telefono) {
+		this.telefono = telefono;
+	}
+	
+	
+	//override de métodos HashCode y equals manualmente
+	@Override
+	public boolean equals (Object obj) {
+		Contacto aux=(Contacto)obj;
+		if((this.nombre.equals(aux.getNombre())) && (this.email.equals(aux.getEmail()))){
+			return true;
+		}else {
+			return false;
+		}
+	}
+	
+	
+	
+	
+	//generar HashCode y equals desde el Eclipse: boton derecho Source/ generate + use "instanceOf"
+	/*@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((email == null) ? 0 : email.hashCode());
+		result = prime * result + ((nombre == null) ? 0 : nombre.hashCode());
+		result = prime * result + telefono;
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (!(obj instanceof Contacto))
+			return false;
+		Contacto other = (Contacto) obj;
+		if (email == null) {
+			if (other.email != null)
+				return false;
+		} else if (!email.equals(other.email))
+			return false;
+		if (nombre == null) {
+			if (other.nombre != null)
+				return false;
+		} else if (!nombre.equals(other.nombre))
+			return false;
+		if (telefono != other.telefono)
+			return false;
+		return true;
+	}*/
+	
+	
+	
+	
+}
---
+++ 19_prueba_conjuntos_HashSet_OverrideEquals&&HashCode_compararObjetos/src/principal/Agenda.java
@@ -0,0 +1,27 @@
+package principal;
+
+import java.util.HashSet;
+
+import model.Contacto;
+
+public class Agenda {
+
+	public static void main(String[] args) {
+		
+		HashSet<Contacto> contactos=new HashSet<Contacto>();
+		
+		contactos.add(new Contacto("contacto1","c1@gmail.com",11));
+		contactos.add(new Contacto("contacto2","c2@gmail.com",22));
+		contactos.add(new Contacto("contacto3","c3@gmail.com",33));
+		contactos.add(new Contacto("contacto3","c3@gmail.com",33));
+		
+		//recorremos conjunto
+		for(Contacto c:contactos) {
+			System.out.println(c.getNombre());
+		}
+		
+		
+	}
+
+}
+

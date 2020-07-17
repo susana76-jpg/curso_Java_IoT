c91511d1d7de2f0536b6fefc57d4cd39247c392a
susana <srico@DESKTOP-G4DK7JT>
2020-07-17 17:35:13 +0200

    Array.sort()/Math.random()

---
+++ 20_loteria_primitiva_Math.Random/src/principal/Loteria.java
@@ -0,0 +1,52 @@
+package principal;
+
+import java.util.Arrays;
+//import java.math.*; => si se quiere importar la clase estática y utilizar sus métodos directamente, sin ver de que clase vienen
+//en vez de =>  Math.random()*6+1 --> random()*6+1
+
+public class Loteria {
+
+	public static void main(String[] args) {
+		int[] combinacion=new int[6];
+		int total=0; //cuenta los números que se han generado hasta el momento
+		int generado;
+		
+		while(total<6) {
+			generado=generarNumero();
+			
+			if(!existente(combinacion,total,generado)) {
+				combinacion[total]=generado;
+				total++; //tenemos uno más
+			}
+		}
+		ordenarArray(combinacion);
+		mostrarCombinacion(combinacion);
+
+	}
+	
+	static int generarNumero() {
+		return (int)(Math.random()*49+1);
+	}
+
+	static boolean existente(int[] combinacion, int total,int generado) {
+		
+		for(int i=0;i<total;i++) {
+			
+			if(combinacion[i]==generado) {
+				return true;
+			}
+		}
+		return false; 
+	}
+	
+	static void ordenarArray(int[] combinacion) {
+		Arrays.sort(combinacion);
+	}
+	
+	static void mostrarCombinacion(int[] combinacion) {
+		for(int numero:combinacion) {
+			System.out.print(numero+", ");
+		}
+	}
+	
+}
